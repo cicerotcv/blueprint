fee74b4f20b43be5672167aff3ec75cd4795df7c
cicerotcv <elm.tiago@gmail.com>
2024-04-23 16:18:31 -0300

    refactor: fromObject function in schema module

--- src/modules/schema/from-object.test.ts
+++ src/modules/schema/from-object.test.ts
@@ -1,23 +1,40 @@
-import { blueprintFactory } from "../../utils/factory";
 import { fromObject } from "./from-object";
 
+const getBlueprint = <T>(value: T) => ({
+  generate: jest.fn().mockReturnValue(value) as () => T,
+  transform: () => ({} as any),
+});
+
+const schema = {
+  age: getBlueprint(25),
+  height: getBlueprint(6),
+};
+
 describe("fromObject", () => {
-  it("should generate the evaluated object schema", () => {
-    // Define the object schema
-    const objectSchema = {
-      name: blueprintFactory(() => "John")(),
-      age: blueprintFactory(() => 25)(),
-      email: blueprintFactory(() => "john@example.com")(),
-    };
+  beforeEach(() => {
+    (schema.age.generate as jest.Mock).mockClear();
+    (schema.height.generate as jest.Mock).mockClear();
+  });
 
-    // Call the fromObject function
-    const evaluated = fromObject(objectSchema);
+  it("should contain the same keys as the schema", () => {
+    const result = fromObject(schema);
 
-    // Assert the generated values
-    expect(evaluated).toEqual({
-      name: "John",
+    expect(Object.keys(result)).toEqual(Object.keys(schema));
+  });
+
+  it("should call the generate function for each key in the schema", () => {
+    fromObject(schema);
+
+    expect(schema.age.generate).toHaveBeenCalledTimes(1);
+    expect(schema.height.generate).toHaveBeenCalledTimes(1);
+  });
+
+  it("should return an object with the correct values", () => {
+    const result = fromObject(schema);
+
+    expect(result).toEqual({
       age: 25,
-      email: "john@example.com",
+      height: 6,
     });
   });
 });
